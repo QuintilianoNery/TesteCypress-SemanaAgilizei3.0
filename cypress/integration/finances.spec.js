7764877f0639f86bc8a07d511b08596f225e4121
Quintiliano <quintiliano@tecsystem.com.br>
2021-03-20 10:29:42 -0300

    Organização do repositório da semana agilizei

---
+++ cypress/integration/finances.spec.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+//fixtures- dados e arquivos de mocks
+//Integrations - nossos arquivos de teste
+//plugins
+//support - Comandos personalizados
+
+context('Dev Finance Agilizei', () => {
+    it('Cadastrar entradas', () => {
+
+    });
+
+
+
+
+    
+    it('Cadastrar saídas', () => {
+        
+    });
+
+    it('Remover entradas e saídas', () => {
+        
+    });
+});
